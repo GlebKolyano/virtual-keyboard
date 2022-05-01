a375f5ca887069b6a32b56790418338280714254
GlebKolyano <glebkolyano38@gmail.com>
2022-05-01 16:21:06 +0300

    feat: create Keyboard class

---
+++ js/Keyboard.js
@@ -0,0 +1,28 @@
+import Key from './Key.js';
+
+export default class Keyboard {
+  constructor(lang) {
+    this.lang = 'en';
+    this.template = [
+      ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace'],
+      ['Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'Delete'],
+      ['CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter'],
+      ['ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight'],
+      ['ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ControlRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight'],
+    ];
+    this.allKeys = {};
+
+    this.textarea = document.createElement('textarea');
+    this.textarea.classList.add('textarea');
+    this.textarea.setAttribute('rows', 10);
+    this.textarea.setAttribute('cols', 70);
+    this.textarea.setAttribute('autofocus', true);
+    this.keyboard = document.createElement('div');
+    this.keyboard.classList.add('keyboard');
+    this.container = document.createElement('div');
+    this.container.classList.add('container');
+
+    this.container.append(this.textarea, this.keyboard);
+    document.body.append(this.container);
+  }
+}
---
+++ script.js
@@ -0,0 +1,3 @@
+import Keyboard from './js/Keyboard.js';
+
+const keyboard = new Keyboard(window.localStorage.getItem('lang'));
